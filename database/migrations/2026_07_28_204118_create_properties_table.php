5b3a0d5ec259f00d69236d811e980abdc7248e4f
Sarah Ghoula <sarah.ghoula@etu.usthb.dz>
2026-07-29 14:04:19 +0100

    add migration

---
+++ database/migrations/2026_07_28_204118_create_properties_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('properties', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constraint()->onDelete('cascade');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->text('description');
+            $table->decimal('price', 15, 2);
+            $table->string('city')->nullable();
+            $table->string('state')->nullable();
+            $table->decimal('area', 10, 2)->nullable();
+            $table->integer('bedrooms')->nullable();
+            $table->integer('bathrooms')->nullable();
+            $table->enum('status', ['for sale', 'for rent'])->default('for sale');
+            $table->boolean('is_featured')->default(false);
+            $table->boolean('is_active')->default(true);
+            $table->string('main_image')->nullable();
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('properties');
+    }
+};
